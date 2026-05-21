08902bb8517148a95ec0fd1d643c4102a87764e3
arthurromanin <arthur.romanin123@gmail.com>
2026-05-21 18:59:46 -0300

    update: adiciona ca.pem

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,15 +1,22 @@
 import pg from 'pg'
 import dotenv from 'dotenv'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 dotenv.config()
 
 const { Pool } = pg
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const caPath = path.join(__dirname, '../certs/ca.pem')
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    require: true,
-    rejectUnauthorized: false,
+    ca: fs.readFileSync(caPath, 'utf8'),
   },
 })
 
